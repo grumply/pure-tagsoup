afd09b2b32d6cc2fcd3e73f2cfceaaf50d1ef226
grumply <sean@grump.ly>
2018-02-19 07:22:24 -0600

    Simplify nix expression.

--- default.nix
+++ default.nix
@@ -1,46 +1,13 @@
-{ compiler ? "ghc821" }:
-
-let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskell = pkgs.haskell // {
-        packages = pkgs.haskell.packages // {
-          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
-            overrides = new: old: rec {
-
-              tlc =
-                new.callPackage ./deps/tlc/tlc.nix { };
-
-              ef =
-                new.callPackage ./deps/ef/ef.nix { };
-
-              ef-base =
-                new.callPackage ./deps/ef-base/ef-base.nix { };
-
-              trivial =
-                new.callPackage ./deps/trivial/trivial.nix { };
-
-              pure =
-                new.callPackage ./deps/pure/pure.nix { };
-
-              pure-tagsoup =
-                new.callPackage ./pure-tagsoup.nix { };
-
-            };
-          };
-        };
-      };
-    };
-  };
-
-  pkgs = import <nixpkgs> { inherit config; };
-
-in
-  { ef = pkgs.haskell.packages.${compiler}.ef;
-    ef-base = pkgs.haskell.packages.${compiler}.ef-base;
-    atomic = pkgs.haskell.packages.${compiler}.atomic;
-    tlc = pkgs.haskell.packages.${compiler}.tlc;
-    trivial = pkgs.haskell.packages.${compiler}.trivial;
-    pure-tagsoup = pkgs.haskell.packages.${compiler}.pure-tagsoup;
-  }
-
+{ mkDerivation, base, containers, ef, ef-base, pure, stdenv
+, tagsoup
+}:
+mkDerivation {
+  pname = "pure-tagsoup";
+  version = "0.6.0.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    base containers ef ef-base pure tagsoup
+  ];
+  description = "Tagsoup compatability for pure";
+  license = stdenv.lib.licenses.bsd3;
+}
