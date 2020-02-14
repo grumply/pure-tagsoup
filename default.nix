b4573de4c7b4b45bf3f35c5e007730aa490db1ad
Sean Hickman <sean@grumply.com>
2020-02-13 23:36:34 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-core, pure-txt, stdenv, tagsoup }:
 mkDerivation {
   pname = "pure-tagsoup";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-core pure-txt tagsoup ];
   homepage = "github.com/grumply/pure-tagsoup";
